4feacdde8a33ed03e393620420c72864fcaa3370
Vibhu Varshney <varshney.vibhu72@gmail.com>
2019-12-02 00:59:40 -0700

    imported the dataset in the jupyter

---
+++ task3.py
@@ -0,0 +1,12 @@
+from nltk.corpus import reuters
+
+class task3:
+    def __init__(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    print(reuters.fileids())
+    # reuters.
+    categs = reuters.categories()
